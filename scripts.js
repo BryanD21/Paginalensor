eb76809958a0af4031e9844a80fe3dbaa38ae097
BryanD21 <bryan5119@gmail.com>
2023-08-12 14:16:52 -0600

    mejoras

--- scripts.js
+++ scripts.js
@@ -1,23 +1,35 @@
-// Asegúrate de incluir jQuery en tu página antes de agregar este código
+$(document).ready(function() {
+  $('.menu-toggle').click(function() {
+    $('.menu').toggleClass('active');
+  });
+
+  $(window).on('scroll', function() {
+    if ($(window).scrollTop() > 100) {
+      $('header').addClass('active');
+    } else {
+      $('header').removeClass('active');
+    }
+  });
+});
+
 
 $(document).ready(function() {
-    // Desplazamiento suave al hacer clic en los enlaces del encabezado
-    $('nav a, .scroll-link').on('click', function(event) {
-      if (this.hash !== '') {
-        event.preventDefault();
-  
-        const hash = this.hash;
-  
-        $('html, body').animate(
-          {
-            scrollTop: $(hash).offset().top
-          },
-          600, // Duración de la animación en milisegundos
-          function() {
-            window.location.hash = hash;
-          }
-        );
-      }
-    });
+  // Desplazamiento suave al hacer clic en los enlaces del encabezado
+  $('nav a, .scroll-link').on('click', function(event) {
+    if (this.hash !== '') {
+      event.preventDefault();
+
+      const hash = this.hash;
+
+      $('html, body').animate(
+        {
+          scrollTop: $(hash).offset().top
+        },
+        600, // Duración de la animación en milisegundos
+        function() {
+          window.location.hash = hash;
+        }
+      );
+    }
   });
-  
+});
